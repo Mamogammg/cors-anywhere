17c764cd37b3a6232d94060b05c37a4bf14004c9
Mamogammg <142676387+Mamogammg@users.noreply.github.com>
2024-12-06 15:24:46 +0100

    Update cors.js

--- api/cors.js
+++ api/cors.js
@@ -1,11 +1,17 @@
 const corsAnywhere = require('cors-anywhere');
 
 const corsServer = corsAnywhere.createServer({
-    originWhitelist: [], // Permite todos los orígenes. Puedes especificar los permitidos aquí.
-    requireHeader: ['origin', 'x-requested-with'],
-    removeHeaders: ['cookie', 'cookie2']
+  originWhitelist: [],  // Permitir todas las solicitudes. O puedes añadir dominios específicos aquí.
+  requireHeader: ['origin', 'x-requested-with'],
+  removeHeaders: ['cookie', 'cookie2']
 });
 
+// Incluir los encabezados CORS en la respuesta
 module.exports = (req, res) => {
-    corsServer.emit('request', req, res);
+  // Agregar los encabezados CORS antes de manejar la solicitud
+  res.setHeader('Access-Control-Allow-Origin', '*');  // Permitir todas las solicitudes de origen
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+
+  corsServer.emit('request', req, res);
 };
